7626550f3e0aaa59a7ff408f6ea6a37db4ef0e9d
Lucia98-ch <chamalucia@gmail.com>
2020-09-16 00:21:37 -0700

    file updates

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -6,7 +6,7 @@ const SignedOutLinks = () => {
         <ul className="right hide-on-med-and-down">
             <li><NavLink to='/appointment' className="links">Book An Appointment</NavLink></li>
             <li><NavLink to='/news' className="links">Blog</NavLink></li>
-            <li><NavLink to='/provider' className="links">Provider SignIn</NavLink></li>
+            <li><NavLink to='/providerSignin' className="links">Provider SignIn</NavLink></li>
             <li><NavLink to='/signup' className="links">Sign In or Create Account</NavLink></li>
         </ul>
     )
